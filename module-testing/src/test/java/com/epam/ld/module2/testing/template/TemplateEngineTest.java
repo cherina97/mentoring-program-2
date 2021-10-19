a0a88474e5aab0a422c595f5094ff8994ffdf9bc
cherina97 <cherina97@gmail.com>
2021-10-19 15:12:42 +0300

    refactoring

--- module-testing/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ module-testing/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -2,6 +2,7 @@ package com.epam.ld.module2.testing.template;
 
 import com.epam.ld.module2.testing.Client;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.isEmptyString;
@@ -9,35 +10,38 @@ import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 public class TemplateEngineTest {
+    private TemplateEngine templateEngine;
+    private Template template;
+
+    @Before
+    public void setUp() {
+        templateEngine = new TemplateEngine();
+        template = new Template();
+    }
 
     @Test
-    public void messageCantBeNull(){
-        TemplateEngine templateEngine = new TemplateEngine();
+    public void messageCantBeNull() {
         String message = templateEngine.generateMessage(new Template(), new Client());
 
         assertNotNull(message);
     }
 
     @Test
-    public void messageCantBeEmpty(){
-        TemplateEngine templateEngine = new TemplateEngine();
+    public void messageCantBeEmpty() {
         String message = templateEngine.generateMessage(new Template(), new Client());
 
         assertThat(message, not(isEmptyString()));
     }
 
     @Test
-    public void templateShouldReturnValue(){
-        Template template = new Template();
+    public void templateShouldReturnValue() {
         String value = template.getMessage();
 
         assertEquals("Some text: #{value}", value);
     }
 
     @Test
-    public void messageShouldBeFromTemplate(){
-        TemplateEngine templateEngine = new TemplateEngine();
-        Template template = new Template();
+    public void messageShouldBeFromTemplate() {
         String message = templateEngine.generateMessage(template, new Client());
 
         assertEquals(template.getMessage(), message);
