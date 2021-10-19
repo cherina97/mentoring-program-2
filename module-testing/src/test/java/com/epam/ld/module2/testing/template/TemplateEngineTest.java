bc9186281cea8606acbe141ba7ed7a9458cb0548
cherina97 <cherina97@gmail.com>
2021-10-19 14:37:23 +0300

    init commit testing module

---
+++ module-testing/src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -0,0 +1,9 @@
+package com.epam.ld.module2.testing.template;
+
+import com.epam.ld.module2.testing.Client;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TemplateEngineTest {
+
+}
