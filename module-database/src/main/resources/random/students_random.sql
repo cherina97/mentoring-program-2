056707f3fba343ca7745b543f5f1bdedc9babe20
cherina97 <cherina97@gmail.com>
2021-11-08 22:02:48 +0200

    added index report

--- module-database/src/main/resources/random/students_random.sql
+++ module-database/src/main/resources/random/students_random.sql
@@ -3,6 +3,6 @@ select
     left (md5(i::text), 10),
     left (md5(i::text), 10),
     DATE '2018-01-01' + (random() * 700):: integer,
-    (random() * 700 + 100000):: integer,
+    (random() * (100000 - 1) + 100000)::integer,
     left (md5(i::text), 15)
 from generate_series(1, 100000) s(i);
