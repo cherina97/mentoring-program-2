d1f715ce6879a99441f7d84cfc7334d76180914b
cherina97 <cherina97@gmail.com>
2021-12-06 22:04:00 +0200

    implemented controllers

--- spting-mvc/src/main/java/controller/EventController.java
+++ spting-mvc/src/main/java/controller/EventController.java
@@ -1,22 +1,91 @@
 package controller;
 
 import facade.BookingFacade;
+import lombok.SneakyThrows;
+import model.Event;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.List;
 
 @Controller
+@RequestMapping("/events")
 public class EventController {
 
     private final BookingFacade bookingFacade;
+    public static final String TEMPLATE = "eventPage";
 
     public EventController(BookingFacade bookingFacade) {
         this.bookingFacade = bookingFacade;
     }
 
-    //getEventById
-    //getEventsByTitle
-    //getEventsForDay
-    //createEvent
-    //updateEvent
-    //deleteEvent
+    @GetMapping("/{id}")
+    public ModelAndView getEventById(@PathVariable long id) {
+        ModelAndView modelAndView = new ModelAndView(TEMPLATE);
 
+        Event eventById = bookingFacade.getEventById(id);
+        if (eventById != null) {
+            modelAndView.addObject("eventModel", eventById);
+        } else {
+            modelAndView.addObject("eventModel", "Event not found with id = " + id);
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/{title}")
+    public ModelAndView getEventsByTitle(@PathVariable String title,
+                                         @RequestParam(required = false, defaultValue = "10") int pageSize,
+                                         @RequestParam(required = false, defaultValue = "1") int pageNum) {
+        ModelAndView modelAndView = new ModelAndView(TEMPLATE);
+
+        List<Event> eventsByTitle = bookingFacade.getEventsByTitle(title, pageSize, pageNum);
+        if (eventsByTitle != null) {
+            modelAndView.addObject("eventModel", eventsByTitle);
+        } else {
+            modelAndView.addObject("eventModel", "Event not found with title = " + title);
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/{day}")
+    public ModelAndView getEventsForDay(@PathVariable @DateTimeFormat(pattern = "dd.MM.yyyy") Date day,
+                                         @RequestParam(required = false, defaultValue = "10") int pageSize,
+                                         @RequestParam(required = false, defaultValue = "1") int pageNum) {
+        ModelAndView modelAndView = new ModelAndView(TEMPLATE);
+
+        List<Event> eventsByDay = bookingFacade.getEventsForDay(day, pageSize, pageNum);
+        if (eventsByDay != null) {
+            modelAndView.addObject("eventModel", eventsByDay);
+        } else {
+            modelAndView.addObject("eventModel", "Event not found by day = " + day);
+        }
+        return modelAndView;
+    }
+
+    @SneakyThrows
+    @PostMapping("/new")
+    public Event createEvent(@RequestBody Event event) {
+        return bookingFacade.createEvent(event);
+    }
+
+    @PostMapping("/update/{id}")
+    public Event updateEvent(@RequestBody Event event, @PathVariable long id) {
+        return bookingFacade.updateEvent(id, event);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ModelAndView deleteEvent(@PathVariable long id) {
+        ModelAndView modelAndView = new ModelAndView(TEMPLATE);
+
+        boolean isEventDeleted = bookingFacade.deleteEvent(id);
+        if (isEventDeleted) {
+            modelAndView.addObject("eventModel", "Event is delete by id = " + id);
+        } else {
+            modelAndView.addObject("eventModel", "Error! Event wasn't delete by id = " + id);
+        }
+        return modelAndView;
+    }
 }
--- spting-mvc/src/main/java/controller/UserController.java
+++ spting-mvc/src/main/java/controller/UserController.java
@@ -1,12 +1,13 @@
 package controller;
 
 import facade.BookingFacade;
+import lombok.SneakyThrows;
 import model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -25,17 +26,64 @@ public class UserController {
 
         User userById = bookingFacade.getUserById(id);
         if (userById != null) {
-            modelAndView.addObject("userById", userById);
+            modelAndView.addObject("userModel", userById);
         } else {
-            modelAndView.addObject("userById", "user not fount with id = " + id);
+            modelAndView.addObject("userModel", "User not found with id = " + id);
         }
         return modelAndView;
     }
 
-    //getUserById
-    //getUserByEmail
-    //getUsersByName
-    //createUser
-    //updateUser
-    //deleteUser
+    @SneakyThrows
+    @GetMapping("/{email}")
+    public ModelAndView getUserByEmail(@PathVariable String email) {
+        ModelAndView modelAndView = new ModelAndView(TEMPLATE);
+
+        User userByEmail = bookingFacade.getUserByEmail(email);
+        if (userByEmail != null) {
+            modelAndView.addObject("userModel", userByEmail);
+        } else {
+            modelAndView.addObject("userModel", "User not found with email = " + email);
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/{name}")
+    public ModelAndView getUsersByName(@PathVariable String name,
+                                       @RequestParam(required = false, defaultValue = "10") int pageSize,
+                                       @RequestParam(required = false, defaultValue = "1") int pageNum) {
+        ModelAndView modelAndView = new ModelAndView(TEMPLATE);
+
+        List<User> usersByName = bookingFacade.getUsersByName(name, pageSize, pageNum);
+        if (usersByName != null) {
+            modelAndView.addObject("userModel", usersByName);
+        } else {
+            modelAndView.addObject("userModel", "Users not found with name = " + name);
+        }
+        return modelAndView;
+    }
+
+    @SneakyThrows
+    @PostMapping("/new")
+    public User createUser(@RequestBody User user) {
+        return bookingFacade.createUser(user);
+    }
+
+    @SneakyThrows
+    @PostMapping("/update/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable long id) {
+        return bookingFacade.updateUser(id, user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ModelAndView deleteUser(@PathVariable long id) {
+        ModelAndView modelAndView = new ModelAndView(TEMPLATE);
+
+        boolean isUserDelete = bookingFacade.deleteUser(id);
+        if (isUserDelete) {
+            modelAndView.addObject("userModel", "User is delete by id = " + id);
+        } else {
+            modelAndView.addObject("userModel", "Error! User wasn't delete by id = " + id);
+        }
+        return modelAndView;
+    }
 }
