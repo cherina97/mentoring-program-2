24bf60a0de719cec20a8053e6095f0a8180c8957
cherina97 <cherina97@gmail.com>
2022-01-11 12:00:41 +0200

    removed Spring Boot module from project

--- spring-boot/src/main/java/com/epam/task2/exception/GlobalApplicationException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.task2.exception;
-
-public class GlobalApplicationException extends RuntimeException {
-
-    public GlobalApplicationException(String message) {
-        super(message);
-    }
-}
